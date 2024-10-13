a46687770cd7dd3e9d7f53e5b0d2751069a9ee9c
leulss <lsvartman@gmail.com>
2024-10-13 17:20:39 -0300

    adicionando os arquivos iniciais

---
+++ converteutf832.c
@@ -0,0 +1,12 @@
+/* Leo Klinger Svartman 2310862 Turma */
+/* Gabriel Martins Mendes 2310862 Turma */
+#include "converteutf832.h"
+
+int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida){
+    unsigned char c;
+    while(!feof(arquivo_entrada)){ 
+        c = fgetc(c);
+        if(c)
+        return 0;
+    }
+}
