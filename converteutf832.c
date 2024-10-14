36533509f985d03aa1fab72d6772807c21d8b330
leulss <lsvartman@gmail.com>
2024-10-13 22:28:50 -0300

    utf32->utf8

--- converteutf832.c
+++ converteutf832.c
@@ -26,7 +26,7 @@ int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida){
         else if ((bytes[0] & 0xE0) == 0xC0) {
             if (fread(bytes + 1, sizeof(unsigned char), 1, arquivo_entrada) != 1)
             {
-                fprintf(stderr, "Erro de leitura: não foi possível ler o segundo byte.\n");
+                fprintf(stderr, "Erro de leitura: não foi possível ler o bytes seguintes.\n");
                 return -1;
             }
             s = s | (bytes[0]&0x1F) << 6; //
@@ -69,77 +69,73 @@ int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida){
 
 int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida){
 
-    int littleEndian = 0, bigEndian = 0;
-	unsigned int utf32, bom;
+    int littleEndian = 0;
+	unsigned int s, bom;
 
-	fread(&bom, sizeof(unsigned int), 1, input);
+	fread(&bom, sizeof(int), 1, arquivo_entrada);
 	printf("BOM: %02x\n", bom);
 
 	if (bom == 0x0000FEFF)
 	{
-		printf("BOM: LE\n");
-		littleEndian = TRUE;
+		littleEndian = 1;
 	}
 	else if (bom == 0xFFFE0000)
 	{
-		printf("BOM: BE\n");
-		bigEndian = TRUE;
+		littleEndian = 0;
 	}
 	else
 	{
-		printf("BOM invalido ou ausente.\n");
+		fprintf(stderr, "Erro: BOM inválido.\n");
 		return -1;
 	}
 
-	while (fread(&utf32, sizeof(unsigned int), 1, input) == 1)
+	while (fread(&s, sizeof(unsigned int), 1, arquivo_saida) == 1)
 	{
-		unsigned char [4]; // byte1..4 (so pra ser mais facil de escrever)
-		if (bigEndian)
+		unsigned char bytes[4];
+		if (!littleEndian)
 		{
-			utf32 = trocaOrdem(utf32);
+			unsigned int b1, b2, b3, b4;
+
+			b1 = 0x000000FF & s;
+			b2 = 0x0000FF00 & s;
+			b3 = 0x00FF0000 & s;
+			b4 = 0xFF000000 & s;
+
+			s = 0;
+
+			s = s | (b1 << 24);
+			s = s | (b2 << 8);
+			s = s | (b3 >> 8);
+			s = s | (b4 >> 24);
 		}
 
-		int caso = classificaCasoUTF32(utf32);
+		if(s <= 0x7F){
+			bytes[0] = s;
+			fwrite(bytes, sizeof(char), 1, arquivo_saida);
+			printf("Escrevendo 1 byte: %02x\n", bytes[0]);//mensagem de debug
+		}
+		else if(s <= 0x7FF){
+			bytes[0] = 0xC0 | (s >> 6); //
+			bytes[1] = 0x80 | (s & 0x3F);
+			fwrite(bytes, sizeof(char), 2, arquivo_saida);
 
-		switch (caso)
-		{
-		case UM_BYTE:
-			b1 = utf32;
-			fwrite(&b1, sizeof(unsigned char), 1, output);
-			printf("Escrevendo 1 byte: %02x\n", b1);//mensagem de debug
-			break;
+			printf("Escrevendo 2 bytes: %02x %02x\n", bytes[0], bytes[1]); //mensagem de debug 
+		}
+		else if(s <= 0xFFFF){
+			bytes[0] = 0xE0 | (s >> 12);
+			bytes[1] = 0x80 | ((s >> 6) & 0x3F);
+			bytes[2] = 0x80 | (s & 0x3F);
+			fwrite(bytes, sizeof(char), 3, arquivo_saida);
 
-		case DOIS_BYTES:
-			b1 = 0b11000000 | (utf32 >> 6); //
-			b2 = 0b10000000 | (utf32 & 0x3F);
-			fwrite(&b1, sizeof(unsigned char), 1, output);
-			fwrite(&b2, sizeof(unsigned char), 1, output);
-
-			printf("Escrevendo 2 bytes: %02x %02x\n", b1, b2); //mensagem de debug 
-			break;
-
-		case TRES_BYTES:
-			b1 = 0xE0 | (utf32 >> 12);
-			b2 = 0x80 | ((utf32 >> 6) & 0x3F);
-			b3 = 0x80 | (utf32 & 0x3F);
-			fwrite(&b1, sizeof(unsigned char), 1, output);
-			fwrite(&b2, sizeof(unsigned char), 1, output);
-			fwrite(&b3, sizeof(unsigned char), 1, output);
-
-			printf("Escrevendo 3 bytes: %02x %02x %02x\n", b1, b2, b3); //mensagem de debug
-			break;
-
-		case QUATRO_BYTES:
-			b1 = 0b11110000 | (utf32 >> 18);
-			b2 = 0b10000000 | ((utf32 >> 12) & 0x3F);
-			b3 = 0b10000000 | ((utf32 >> 6) & 0x3F);
-			b4 = 0b10000000 | (utf32 & 0x3F);
-			fwrite(&b1, sizeof(unsigned char), 1, output);
-			fwrite(&b2, sizeof(unsigned char), 1, output);
-			fwrite(&b3, sizeof(unsigned char), 1, output);
-			fwrite(&b4, sizeof(unsigned char), 1, output);
-			printf("Escrevendo 4 bytes: %02x %02x %02x %02x\n", b1, b2, b3, b4); //mensagem de debug
-			break;
+			printf("Escrevendo 3 bytes: %02x %02x %02x\n", bytes[0], bytes[1], bytes[2]); //mensagem de debug
+		}
+		else if(s <= 0x10FFFF){
+			bytes[0] = 0xF0 | (s >> 18);
+			bytes[1] = 0x80 | ((s >> 12) & 0x3F);
+			bytes[2] = 0x80 | ((s >> 6) & 0x3F);
+			bytes[3] = 0x80 | (s & 0x3F);
+			fwrite(bytes, sizeof(char), 4, arquivo_saida);
+			printf("Escrevendo 4 bytes: %02x %02x %02x %02x\n", bytes[0], bytes[1], bytes[2], bytes[3]); //mensagem de debug
 		}
 	}
 
